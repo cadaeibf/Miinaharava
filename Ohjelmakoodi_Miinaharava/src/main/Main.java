aba23767abb0b3d4d140a01746ef5bae02608c93
cadaeibf <antero.vainio@helsinki.fi>
2013-10-16 12:39:40 +0300

    muokkausta koodiin ja jar-tiedosto rakennettu

--- Ohjelmakoodi_Miinaharava/src/main/Main.java
+++ Ohjelmakoodi_Miinaharava/src/main/Main.java
@@ -1,9 +1,6 @@
 
 package main;
 
-import javax.swing.SwingUtilities;
-import ohjelmalogiikka.Asetukset;
-
 /**
  * Pääluokka käynnistää ohjelman kutsumalla graafista käyttöliittymää
  * @author Cadaei
@@ -13,9 +10,6 @@ public class Main {
     public static void main(String[] args) {
         AsetuksetRuutu asetuksetRuutu = new AsetuksetRuutu();
         asetuksetRuutu.run();
-        
-//        Kayttoliittyma kayttoliittyma = new Kayttoliittyma(new Asetukset(40,32,200));
-//        SwingUtilities.invokeLater(kayttoliittyma);
         
     }
 }
